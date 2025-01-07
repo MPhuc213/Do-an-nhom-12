ef9edeec9671d7d99bcc264a432a40fdacc91fd5
MPhuc213 <minhphuc21042005@gmail.com>
2025-01-07 16:01:28 +0700

    insert database

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,3 +1,12 @@
+import mysql2 from 'mysql2';
+
+const connection = mysql2.createConnection({
+    host: 'localhost',
+    user: 'root',        // Tên người dùng MySQL
+    database: 'jwt' // Tên cơ sở dữ liệu
+});
+
+
 const handleHome = (req,res) => {
     return res.render("Home.ejs");
 }
@@ -5,6 +14,25 @@ const handleHome = (req,res) => {
 const handleUserPage = (req,res) => {
     //moudule from database
     return res.render("User.ejs");
+}
+
+const handelUserCreate = (req,res) => {
+    let email = req.body.email;
+    let username = req.body.username;
+    let password = req.body.password;
+    let phonenumber = req.body.PhoneNumber;
+
+    
+
+    connection.query(
+        'INSERT INTO users (username, password, email, phone) VALUES (?, ?, ?, ?)',[username, password, email, phonenumber],
+        function (err, results,fields){
+            console.log(results);
+            console.log(fields);
+        }
+    )
+    console.log(">>> Check request ", req.body);
+    return res.send("handelUserCreate");
 }
 
 const handleregister = (req,res) => {
@@ -17,5 +45,5 @@ const handlelogin = (req,res) => {
 
 
 module.exports = {
-    handleHome, handleUserPage, handleregister, handlelogin
+    handleHome, handleUserPage, handelUserCreate, handleregister, handlelogin
 }
--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import configViewEngine from "./configs/viewEngine.js";
 import initWebRoutes from "./routes/web.js";
+import bodyParser from 'body-parser';
+
 require("dotenv").config();
 
 
@@ -8,6 +10,10 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 //config view engine
 configViewEngine(app);
+
+//config body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 
 //init web routes
 initWebRoutes(app);
